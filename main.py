441aee6d413b7dc4bd50b68f578bbbc2d880c1a7
gi613ts <malkowski.m.a@gmail.com>
2025-08-06 19:22:41 +0200

    3.1 Print a Report

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from stats import get_num_words, get_num_chars
 
-word_count =  get_num_words("books/frankenstein.txt")
-print(f"{word_count} words found in the document")
+book_path = "books/frankenstein.txt"
 
-char_count = get_num_chars("books/frankenstein.txt")
-print(char_count)
+word_count =  get_num_words(book_path)
+char_count = get_num_chars(book_path)
+
+#print(char_count)
+
+char_list = []
+for item in char_count:
+    char_list.append({"char":item, "num":char_count[item]})
+
+def sort_key(dict_item):
+    return dict_item["num"]
+
+char_list.sort(reverse=True, key=sort_key)
+
+#print(char_list)
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_path}")
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")
+print("--------- Character Count -------")
+for char_dict in char_list:
+    if char_dict["char"].isalpha():
+        print(f"{char_dict["char"]}:{char_dict["num"]}")
+print("============= END ===============")
