010251b283b258e7d8104b10a9d16b633ba99a1f
gi613ts <malkowski.m.a@gmail.com>
2025-08-06 18:41:14 +0200

    2.6 Count Characters

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 word_count =  get_num_words("books/frankenstein.txt")
 print(f"{word_count} words found in the document")
+
+char_count = get_num_chars("books/frankenstein.txt")
+print(char_count)
--- stats.py
+++ stats.py
@@ -7,3 +7,18 @@ def get_num_words(filepath):
         book_text = book_file.read()
         word_count = len(book_text.split())
     return word_count
+
+def get_num_chars(filepath):
+    '''
+    takes a filepath to a book in .txt,
+    returns a dict counting each character
+    '''
+    with open(filepath) as book_file:
+        book_text = book_file.read().lower()
+        char_dict = {}
+        for char in book_text:
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+    return char_dict
